8bd6bcdf41ff823fc3b21eeaff9626637b5fb1ef
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-09-08 05:24:15 -0400

    finished html get routes

--- server.js
+++ server.js
@@ -12,22 +12,33 @@ app.use(express.json());
 
 app.use(express.static('./public'));
 
-app.get('api/notes', (req,res) => {
-
+app.get('notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('api/notes', (req,res) => {
+app.get('notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-})
+app.get('api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, './public/notes.html'));
+    let apiRoutes = JSON.parse(data);
+    if  (apiRoutes) {
+        res.json(apiRoutes);
+    }
+    else {
+        res.send(404);
+    }
+});
 
 app.post('api/notes', (req, res) =>{
-
-})
+    
+});
 
 app.delete('/api/notes/:id', (req, res) => {
 
-})
+});
 
 app.listen(PORT, () => {
-    
-})
+    console.log(`API server now on port ${PORT}!`);
+});
